01f252a009812986e0dc60c9756bab8f40e25266
nikhilkuria <nikhilkuria@gmail.com>
2017-01-07 11:01:26 +0530

    small mods

--- classObjectFunctions/TaskRunner.php
+++ classObjectFunctions/TaskRunner.php
@@ -10,42 +10,49 @@ $task->doTask();
 /*
 Adding extra checks to see the file and the class exists
 */
+try{
+  //A positive case
+  $className = "HardTask";
+  $fileName = "tasks/${className}.php";
+  if(!file_exists($fileName)){
+    throw new Exception("No such file found in the path : {$fileName}");
+  }
 
-//A positive case
-$className = "HardTask";
-$fileName = "tasks/${className}.php";
-if(!file_exists($fileName)){
-  throw new Exception("No such file found in the path : {$fileName}");
+  require_once $fileName;
+  $qClassName = "tasks\\${className}";
+  if(!class_exists($qClassName)){
+    throw new Exception("No such class : {$qClassName} in the path {$fileName}");
+  }
+
+  $speaker = new $qClassName();
+  $speaker->doTask();
+
+  //A negative case where the file is not presemt
+  $className2 = "HardTask123";
+  $fileName2 = "tasks/${className}.php";
+  if(!file_exists($fileName)){
+    throw new Exception("No such file found in the path : {$fileName}");
+  }
+
+  //A negative case where the file is present but the class is not r=prest in the namespae
+  $className3 = "HardTask";
+  $className = "NoTask";
+  $fileName3 = "tasks/${className}.php";
+  if(!file_exists($fileName)){
+    throw new Exception("No such file found in the path : {$fileName}");
+  }
+
+  require_once $fileName;
+  $qClassName = "tasks\\${className}";
+  if(!class_exists($qClassName)){
+    throw new Exception("No such class : {$qClassName} in the path {$fileName}");
+  }
+}
+catch(Exception $ex){
+  print $ex->getMessage();
 }
 
-require_once $fileName;
-$qClassName = "tasks\\${className}";
-if(!class_exists($qClassName)){
-  throw new Exception("No such class : {$qClassName} in the path {$fileName}");
-}
+print "\nPrinting all the declared classes now!\n"
 
-$speaker = new $qClassName();
-$speaker->doTask();
-
-//A negative case where the file is not presemt
-$className2 = "HardTask123";
-$fileName2 = "tasks/${className}.php";
-if(!file_exists($fileName)){
-  throw new Exception("No such file found in the path : {$fileName}");
-}
-
-//A negative case where the file is present but the class is not r=prest in the namespae
-$className3 = "HardTask";
-$className = "NoTask";
-$fileName3 = "tasks/${className}.php";
-if(!file_exists($fileName)){
-  throw new Exception("No such file found in the path : {$fileName}");
-}
-
-require_once $fileName;
-$qClassName = "tasks\\${className}";
-if(!class_exists($qClassName)){
-  throw new Exception("No such class : {$qClassName} in the path {$fileName}");
-}
-
+print_r(get_declared_classes());
  ?>
--- interceptors/autoLoadInterceptor.php
+++ interceptors/autoLoadInterceptor.php
@@ -4,16 +4,18 @@
 
 class Writer{
   public function helloWorld(){
-    $writer = new HelloWorldWriter();
     print "Hello World!";
-  }
-
-  function __autoload($className){
-    print "Attempt to load {$className}\n";
   }
 }
 
+
+function __autoload($className){
+  print "Attempt to load {$className}\n";
+}
+
 $writer = new Writer();
+$writer = new HelloWorldWriter();
+
 $writer->helloWorld();
 
  ?>
