9fc371913ea10279a0f5ec061e1fc4891e27fa06
nikhilkuria <nikhilkuria@gmail.com>
2017-01-15 10:47:50 +0530

    completing first tutorial

--- symfony/aqua_note/src/AppBundle/Controller/GenusController.php
+++ symfony/aqua_note/src/AppBundle/Controller/GenusController.php
@@ -4,8 +4,10 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -53,4 +55,21 @@ class GenusController extends Controller
         return new Response($html);
     }
 
+    /**
+     * @Route("/genus/{genusName}/notes", name = "genus_show_notes")
+     */
+    public function getNotes($genusName){
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes' => $notes
+        ];
+
+        //return new Response(json_encode($data));
+        return new JsonResponse($data);
+    }
 }
