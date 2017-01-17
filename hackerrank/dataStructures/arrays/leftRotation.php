46c8a854bca6235c47852cdbe82f1136b0409000
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2017-01-17 05:30:43 +0000

    left rotation almost right. a few test cases fail

--- hackerrank/dataStructures/arrays/leftRotation.php
+++ hackerrank/dataStructures/arrays/leftRotation.php
@@ -1,62 +1,42 @@
 <?php
 
 $handle = fopen("php://stdin", "r");
-fscanf($handle, "%d", $arrayLength);
-fscanf($handle, "%d", $numOfRota);
-$arrayString = fgets($handle);
+$inputString =explode(" ", trim(fgets($handle)));
+$arrayLength = $inputString[0];
+$numOfRota = $inputString[1];
+$arrayString = trim(fgets($handle));
 $array = explode(" ", $arrayString);
 
 $swapCache = array();
 
 function getToLocation($startIndex, $numOfRota, $arrayLength){
     if($startIndex-$numOfRota<0){
-        return $arrayLength - (abs($startIndex-$numOfRota+1));
+        return $arrayLength - (abs($startIndex-$numOfRota+1)) - 1;
     }
-    print("{$startIndex} {$numOfRota}");
     return $startIndex-$numOfRota;
 }
 
 function swap(&$array, &$swapCache, $numOfRota){
-    $indexToSwap = &$swapCache[0];
-    $valToSwap = &$swapCache[1];
+    $indexToSwap = $swapCache[0];
+    $valToSwap = $swapCache[1];
+    //print("About to swap {$indexToSwap} with value {$valToSwap} ");
     
-    $tempValue = $array[$indexToSwap];
+    $toLocation = getToLocation($indexToSwap, $numOfRota, count($array));
+    //print("to location index {$toLocation}\n");
     
-    $array[$indexToSwap] = $valToSwap;
-    $valToSwap = $tempValue;
-    $indexToSwap = getToLocation($indexToSwap, $numOfRota, count($array));
+    $swapCache[0] = $toLocation;
+    $swapCache[1] = $array[$toLocation];
+    
+    $array[$toLocation] = $valToSwap;
 }
 
 $swapCache[0] = 0;
-$swapCache[1] = 0;
+$swapCache[1] = $array[0];
 
-print_r($array);
-print_r($swapCache);
-swap($array, $swapCache, $numOfRota);
-print_r($array);
-print_r($swapCache);
+for($index = 0;$index< count($array);$index++){
+        swap($array, $swapCache,$numOfRota);
+}
 
-print_r($array);
-print_r($swapCache);
-swap($array, $swapCache, $numOfRota);
-print_r($array);
-print_r($swapCache);
-
-print_r($array);
-print_r($swapCache);
-swap($array, $swapCache, $numOfRota);
-print_r($array);
-print_r($swapCache);
-
-
-/*do{
-    print_r($array);
-    print_r($swapCache);
-    swap($array, $swapCache,$numOfRota);
-    
-}while($swapCache[0] != 0);*/
-
-
-
+print(implode(" ",$array));
 
 ?>
