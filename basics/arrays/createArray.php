d613a066a352cd860326288590cb24c7601539b1
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2017-02-03 04:22:12 +0000

    simple array init examples

---
+++ basics/arrays/createArray.php
@@ -0,0 +1,9 @@
+<?php
+$fruits = array (
+    "fruits"  => array("a" => "Orange", "b" => "Banane", "c" => "Apfel"),
+    "numbers" => array(1, 2, 3, 4, 5, 6),
+    "holes"   => array("erstes", 5 => "zweites", "drittes")
+);
+
+print_r($fruits);
+?>
