42e9b46ff5148e2b7c316fb35d00835c76ff704c
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2017-02-08 12:08:52 +0000

    checking metadata using curl

--- curl/simpleDemo.php
+++ curl/simpleDemo.php
@@ -1,4 +1,7 @@
 <?php
+
+//https://code.tutsplus.com/tutorials/techniques-for-mastering-curl--net-8470
+
 //step1
 $cSession = curl_init(); 
 //step2
@@ -7,8 +10,15 @@ curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($cSession,CURLOPT_HEADER, false); 
 //step3
 $result=curl_exec($cSession);
+
+//also can get info
+
+$metadata=curl_getinfo($cSession);
 //step4
 curl_close($cSession);
 //step5
 echo $result;
+print_r($metadata);
+echo 'Took ' . $metadata['total_time'] . ' seconds for url ' . $metadata['url']."\n";
+
 ?>
