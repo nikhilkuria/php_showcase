ea1379d60f3cffd085857a4756b137a521d75217
nikhilkuria <nikhilkuria@gmail.com>
2017-03-16 21:53:20 +0100

    working with listeners

--- symfony_demo/intro/src/NewBundle/Listeners/RequestListener.php
+++ symfony_demo/intro/src/NewBundle/Listeners/RequestListener.php
@@ -9,7 +9,13 @@
 namespace NewBundle\Listeners;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
 class RequestListener
 {
-
+    public function onKernelRequest(GetResponseEvent $event){
+        //$response = new Response("Intercepted");
+        //$event->setResponse($response);
+    }
 }
